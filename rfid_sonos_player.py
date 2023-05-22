6e1a62d2b93111f441a242aedeb2854a3622472c
James Sinclair <sinclair.j@gmail.com>
2023-05-22 17:01:13 +1000

    Tidy up, add readme and gitignore

--- rfid_sonos_player.py
+++ rfid_sonos_player.py
@@ -1,26 +1,36 @@
 from PiicoDev_RFID import PiicoDev_RFID
 from PiicoDev_Unified import sleep_ms
-import requests                                 # To use request package in current program 
+import requests                                 
 
 rfid = PiicoDev_RFID()
 
-myString = "test"
+# sonos group of speakers to play
+sonos_group = "Bedroom"
+sonos_user = "James"
+
+record_string = ""
 print('Hold tag near the PiicoDev RFID Module to read some text')
-load = requests.get("http://localhost:5005/Jonathan/musicsearch/library/load")        
+load = requests.get(f"http://localhost:5005/{sonos_user}/musicsearch/library/load")    
+print(load)
 
 while True:
-    myString_new = rfid.readText()
-    print("string_new " +myString_new)
-    print("string " + myString)
+    record_string_new = rfid.readText()
+    print("string_new " +record_string_new)
+    print("string " + record_string)
     sleep_ms(1000)
-    if myString != myString_new and myString_new != "":
-        myString = rfid.readText()
-        response = requests.get("http://localhost:5005/Living Room/" + myString)        # To execute get request 
-        print(response.status_code)     # To print http response code  
-        print(response.text)            # To print formatted JSON response 
-        sleep_ms(1000)
-        response = requests.get("http://localhost:5005/Living Room/play")        # To execute get request 
+    if record_string != record_string_new and record_string_new != "":
+        record_string = rfid.readText()
         print('Text in tag:')
-        print(myString)
+        print(record_string)
+
+        response = requests.get(f"http://localhost:5005/{sonos_group}/{record_string}")        
+        # only play when successful
+        if response.status_code == 200:     # To print http response code  
+            sleep_ms(1000)
+            response = requests.get(f"http://localhost:5005/{sonos_group}/play")        # To execute get request 
+        else:
+            print(response.status_code)
+            print(response.text)            # To print formatted JSON response 
+
         sleep_ms(2000)
 
